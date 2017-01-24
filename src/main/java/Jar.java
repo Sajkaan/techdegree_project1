45c4bbcd712ccab4ea8c5d8731715b0409c3979a
Sajkaan <safet.garic@hotmail.com>
2017-01-24 23:36:58 +0100

    Moved check and  higherOrLower methods to prompter.

--- src/main/java/Jar.java
+++ src/main/java/Jar.java
@@ -6,7 +6,6 @@ public class Jar {
   private String item;
   private int maxNumberOfItemsInTheJar;
   private int randomNumberOfItemsInTheJar;
-  private int numberOfGuesses = 0;
 
   public Jar(String item, int maxNumberOfItemsInTheJar) {
     this.item = item;
@@ -37,41 +36,10 @@ public class Jar {
     return randomNumberOfItemsInTheJar;
   }
 
-  public int getNumberOfGuesses() {
-    return numberOfGuesses;
-  }
 
   // Inserts a random number in the jar
   public void fill() {
     Random random = new Random();
     this.randomNumberOfItemsInTheJar = random.nextInt(maxNumberOfItemsInTheJar) + 1;
-  }
-
-  // Checks if the guess is right, throws Exception if the input is bellow 0 or higher than the maximum
-  public boolean check(int guess) {
-    if (guess <= 0) {
-      throw new IllegalArgumentException(
-          "The guess should be a positive number and bigger than 0.");
-    } else if (guess > maxNumberOfItemsInTheJar) {
-      throw new IllegalArgumentException(
-          "Your guess must be less than " + maxNumberOfItemsInTheJar);
-    } else {
-      numberOfGuesses++;
-      if (guess == randomNumberOfItemsInTheJar) {
-        return true;
-      } else {
-        higherOrLower(guess);
-        return false;
-      }
-    }
-  }
-
-  // Checks if the guess is higher or lower than the searched number
-  public void higherOrLower(int guess) {
-    if (guess > randomNumberOfItemsInTheJar) {
-      System.out.println("Your guess is too high.");
-    } else {
-      System.out.println("Your guess is too low.");
-    }
   }
 }
--- src/main/java/Prompter.java
+++ src/main/java/Prompter.java
@@ -6,11 +6,11 @@ public class Prompter {
   private String name;
   private String nameOfItem;
   private int max;
+  private int numberOfGuesses = 0;
   Scanner input = new Scanner(System.in);
   private Jar jar;
 
   public Prompter() {
-
   }
 
   public void jarSetup() {
@@ -29,7 +29,7 @@ public class Prompter {
 
     // Asks for the number of your items, if the number is not valid it repeats
     do {
-      System.out.printf("Maximum number of %s in the jar: ", nameOfItem);
+      System.out.printf("Maximum number of %s(s) in the jar: ", nameOfItem);
       try {
         max = input.nextInt();
         jar = new Jar(nameOfItem, max);
@@ -52,32 +52,62 @@ public class Prompter {
     System.out.println("\n\n" + jar.getChallenger());
     System.out.println("======================");
     System.out.printf(
-        "Your goal is to guess how many %s are in the jar. Your guess should be between 1 and %d\n",
+        "Your goal is to guess how many %s(s) are in the jar. Your guess should be between 1 and %d.\n",
         jar.getItem(),
         jar.getMaxNumberOfItemsInTheJar());
     promptEnterKey();
 
     // Checks the guess for a valid number and if your guess is right
     do {
-      System.out.print("Guess: ");
+      System.out.printf("How many %s(s) are in the jar? Pick a number between 1 and %d: ",
+          jar.getItem(),
+          jar.getMaxNumberOfItemsInTheJar());
       try {
-        isGuessed = jar.check(input.nextInt());
+        isGuessed = check(input.nextInt());
         isPositive = true;
       } catch (IllegalArgumentException iae) {
         System.out.println("Please try again. " + iae.getMessage());
       }
     } while (!isGuessed || !isPositive);
 
-    System.out.printf("Congrats you needed %d attempts to guess it.", jar.getNumberOfGuesses());
+    System.out.printf("Congrats you needed %d attempt(s) to guess it.", numberOfGuesses);
+  }
+
+  // Checks if the guess is higher or lower than the searched number
+  public void higherOrLower(int guess) {
+    if (guess > jar.getRandomNumberOfItemsInTheJar()) {
+      System.out.println("Your guess is too high.");
+    } else {
+      System.out.println("Your guess is too low.");
+    }
   }
 
 
   public static void promptEnterKey() {
-    System.out.println("Ready? (press \"ENTER\" to star guessing)");
+    System.out.println("Ready? (press \"ENTER\" to start guessing)");
     try {
       int read = System.in.read(new byte[2]);
     } catch (IOException e) {
       e.getMessage();
+    }
+  }
+
+  // Checks if the guess is right, throws Exception if the input is bellow 0 or higher than the maximum
+  public boolean check(int guess) {
+    if (guess <= 0) {
+      throw new IllegalArgumentException(
+          "The guess should be a positive number and bigger than 0.");
+    } else if (guess > jar.getMaxNumberOfItemsInTheJar()) {
+      throw new IllegalArgumentException(
+          "Your guess must be equal or less than " + jar.getMaxNumberOfItemsInTheJar());
+    } else {
+      numberOfGuesses++;
+      if (guess == jar.getRandomNumberOfItemsInTheJar()) {
+        return true;
+      } else {
+        higherOrLower(guess);
+        return false;
+      }
     }
   }
 }
